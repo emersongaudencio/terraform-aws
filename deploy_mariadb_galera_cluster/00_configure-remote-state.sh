3c7e1f07323663430aa36d323560f494e003378f
Emerson Gaudencio <emerson.fender@gmail.com>
2020-11-01 03:13:09 +0000

    enabling terraform remote backend state being stored on S3 bucket with versioning.

---
+++ deploy_mariadb_galera_cluster/00_configure-remote-state.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# backend remote state
+echo 'terraform {
+  backend "s3" {
+    bucket = "terraform-state-turbo-dba-prod"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+' > backend.tf
